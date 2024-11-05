b5a91fa718f37e6cba2ffb3f5b48c7400c22478a
Ludo Pulles <ludo.pulles@gmail.com>
2024-11-05 13:49:17 +0100

    Benchmark fixed `q` and chain commands

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,11 +7,15 @@ from flatter_conversion import convert_logfiles
 
 
 # Specify which lattices we want to test:
-mlgqs = [(128, 10), (256, 20)]  #, (512, 36)]
-# mlgqs = [(512, 32)]
+mqs = [
+    (128, 631),  # latticegen q 2 1 10 p
+    (256, 829561),  # latticegen q 2 1 20 p
+    (512, 968665207),  # latticegen q 2 1 30 p
+    (1024, 829561),  # latticegen q 2 1 20 p
+]
 
 seeds = range(10)
-lattices = [(m, lgq, seed, f"../input/{m}_{lgq}_{seed}") for (m, lgq) in mlgqs for seed in seeds]
+lattices = [(m, q, seed, f"../input/{m}_{q}_{seed}") for (m, q) in mqs for seed in seeds]
 
 
 def run_command(cmd):
@@ -19,39 +23,39 @@ def run_command(cmd):
     return subprocess.run(cmd, shell=True)
 
 
-def gen_lattice(m, lgq, seed, path):
+def gen_lattice(m, q, seed, path):
     n = m//2
-    result = run_command(f"latticegen -randseed {seed} q {m} {n} {lgq} p > {path}")
+    result = run_command(f"latticegen -randseed {seed} q {m} {n} {q} q > {path}")
     if result.returncode != 0:
         print(result.stderr)
         exit(1)
 
 
-def run_seysenlll(m, lgq, seed, path):
-    logfile = f"logs/lll_{m}_{lgq}_{seed}.csv"
+def run_seysenlll(m, q, seed, path):
+    logfile = f"logs/lll_{m}_{q}_{seed}.csv"
     result = run_command(f"../src/app.py -pqvi {path} --logfile {logfile}")
     if result.returncode != 0:
         print(result.stderr)
         os.remove(logfile)
-        # exit(1)
 
 
-def run_seysendeeplll(m, lgq, seed, path, depth):
-    logfile = f"logs/deeplll_d{depth}_{m}_{lgq}_{seed}.csv"
+def run_seysendeeplll(m, q, seed, path, depth):
+    logfile = f"logs/deeplll_d{depth}_{m}_{q}_{seed}.csv"
     result = run_command(f"../src/app.py -pqvi {path} --depth {depth} --logfile {logfile}")
     if result.returncode != 0:
         print(result.stderr)
         os.remove(logfile)
-        # exit(1)
 
 
-def run_flatter(m, lgq, seed, path):
-    flogfile = f"flatter_logs/{m}_{lgq}_{seed}.log"
-    result = run_command(f"FLATTER_LOG={flogfile} ~/.local/bin/flatter -q {path}")
+def run_flatter(m, q, seed, path, num_threads):
+    flogfile = f"flatter_logs/{m}_{q}_{seed}.log"
+    cmd = f"OMP_NUM_THREADS={num_threads} FLATTER_LOG={flogfile} ~/.local/bin/flatter -q {path}"
+    result = run_command(cmd)
     if result.returncode != 0:
         print(result.stderr)
         exit(1)
-    plogfile = f"logs/flatter_{m}_{lgq}_{seed}.csv"
+
+    plogfile = f"logs/flatter_{m}_{q}_{seed}.csv"
     convert_logfiles(flogfile, plogfile)
 
 
@@ -73,40 +77,42 @@ def benchmark(cmd):
 
 
 def __main__():
-    if '--lattices' in sys.argv[1:]:
+    has_cmd = 'lattices' in sys.argv[1:]
+    if has_cmd:
         for lat in lattices:
             gen_lattice(*lat)
 
-    if '--seysen' in sys.argv[1:]:
-        for lat in lattices:
-            run_seysenlll(*lat)
+    for i, arg in enumerate(sys.argv[1:]):
+        is_cmd = True
+        if arg == 'dim':
+            assert 2 + i < len(sys.argv), "dim param expected!"
+            dim = int(sys.argv[2 + i])
+            assert dim in [m for (m, q) in mqs], "Unknown dimension"
+            curq = [q for (m, q) in mqs if m == dim]
+            assert len(curq) == 1
+            curq = curq[0]
+            lattices = [(dim, curq, seed, f"../input/{dim}_{curq}_{seed}") for seed in seeds]
 
-    if '--depth' in sys.argv[1:]:
-        depth = int(sys.argv[2 + sys.argv[1:].index('--depth')])
-        for lat in lattices:
-            run_seysendeeplll(*lat, depth)
+        elif arg == 'seysen':
+            for lat in lattices:
+                run_seysenlll(*lat)
+        elif arg == 'depth':
+            assert 2 + i < len(sys.argv), "depth param expected!"
+            depth = int(sys.argv[2 + i])
+            for lat in lattices:
+                run_seysendeeplll(*lat, depth)
+        elif arg == 'flatter':
+            assert 2 + i < len(sys.argv), "num_threads param expected!"
+            num_threads = int(sys.argv[2 + i])
+            for lat in lattices:
+                run_flatter(*lat, num_threads)
+        else:
+            is_cmd = False
+        has_cmd = has_cmd or is_cmd
 
-    if '--flatter' in sys.argv[1:]:
-        for lat in lattices:
-            run_flatter(*lat)
+    if not has_cmd:
+        print(f"Usage: {sys.argv[0]} [dim d|lattices|seysen|depth d|flatter num_threads]")
 
 
 if __name__ == "__main__":
     __main__()
-#    print('   m,    n,  q,    method, real, user, sys')
-#    for m in range(16, 1024+1, 16):
-#        n = m // 2
-#        q = 32 # round(m / 12 + 5)
-#
-#        # Run SeysenLLL
-#        name, cmd = 'SeysenLLL', f'time {{ latticegen q {m} {n} {q} p | ../src/app.py -q;}}'
-#        print(f'{m:4}, {n:4}, {q:2}, {name}, ', flush=True, end='')
-#        print(benchmark(cmd), flush=True)
-#        # # Run Flatter
-#        # name, cmd = '  Flatter', f'~/.local/bin/flatter -q {file}'
-#        # print(f'{m:4}, {n:4}, {q:2}, {name}, ', end='')
-#        # print(benchmark(cmd), end='', flush=True)
-#        # # Run FPLLL
-#        # name, cmd = '    fpLLL', f'fplll {file} > /dev/null'
-#        # print(f'{m:4}, {n:4}, {q:2}, {name}, ', end='')
-#        # print(benchmark(cmd), end='', flush=True)
