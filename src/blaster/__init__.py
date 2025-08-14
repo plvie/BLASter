2d593d6f241a28dc6bf7c300cd692c1161b2ec9a
Paul Vie <paul.vie@telecom-paris.fr>
2025-08-14 19:15:00 +0200

    feat: fast babai reduction batched on GPU

--- src/blaster/__init__.py
+++ src/blaster/__init__.py
@@ -1,5 +1,5 @@
 from .lattice_io import read_qary_lattice, write_lattice
 from .blaster import reduce
 from .stats import gaussian_heuristic, rhf, slope, get_profile
-from .size_reduction import is_lll_reduced, is_weakly_lll_reduced, size_reduce, seysen_reduce
-from .size_reduction_gpu import is_weakly_lll_reduced_gpu, seysen_reduce_gpu
+from .size_reduction import is_lll_reduced, is_weakly_lll_reduced, size_reduce, seysen_reduce, babai_last_qr_on_the_fly
+from .size_reduction_gpu import is_weakly_lll_reduced_gpu, seysen_reduce_gpu, babai_last_gpu_batched
